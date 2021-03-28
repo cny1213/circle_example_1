291df9963a53f82be7dffd16d067813664648f1c
cny <cny1213@naver.com>
2021-03-29 08:15:12 +0900

    circle_example
    week04

---
+++ src/context.h
@@ -0,0 +1,27 @@
+#ifndef __CONTEXT_H__
+#define __CONTEXT_H__
+
+#include "common.h"
+#include "shader.h"
+#include "program.h"
+#include "buffer.h"
+#include "vertex_layout.h"
+
+CLASS_PTR(Context)
+class Context {
+public:
+    static ContextUPtr Create();
+    void CreatCircle(float radius, int segment);
+    void Render();    
+private:
+    Context() {}
+    bool Init();
+    ProgramUPtr m_program;
+
+    VertexLayoutUPtr m_vertexLayout;
+    BufferUPtr m_vertexBuffer;
+    BufferUPtr m_indexBuffer;
+    int m_indexCount { 6 };
+};
+
+#endif // __CONTEXT_H__
